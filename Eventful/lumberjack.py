014e0b1897f4edfc70b409fe7023e9fd3b263c41
cas1m1r <50683460+cas1m1r@users.noreply.github.com>
2024-02-04 16:50:59 -0500

    Update lumberjack.py

--- Eventful/lumberjack.py
+++ Eventful/lumberjack.py
@@ -1,67 +1,63 @@
-import random
-import string
+from evtx import PyEvtxParser as pevt
 import json
+import time
 import os
 
-lets = list(string.ascii_letters)
-
-def random_file():
-    return ''.join(random.sample(lets, 4))
-
-def run(cmd):
-    tmp = random_file()
-    os.system(f'{cmd} > {tmp}')
-    with open(tmp,'r') as f:
-        result = f.read()
-    f.close()
-    os.remove(tmp)
-    return result
-
-def non_empty(elmts):
-    items = []
-    for e in elmts:
-        if len(e):
-            items.append(e)
-    return items
+def log_read(evt):
+    parser = pevt(evt)
+    log_data = []
+    windows_events = load_event_types()
+    for record in parser.records():
+        data = parse_xml(record['data'])
+        if 'EventID' in data.keys():
+            event_id = data['EventID']
+        elif 'EventRecordID' in data.keys():
+            event_id = data['EventRecordID']
+        else:
+            event_id = ''
+        if event_id in windows_events.keys():
+            print(f'[{record["timestamp"]}] {windows_events[event_id]}')
+            log_data.append({'time': record['timestamp'], 'description': windows_events[event_id]})
+    return log_data
 
 
-def load_event_types(ev_path):
+def parse_xml(data):
+    structure = {}
+    for x in data.split('\n'):
+        tag = x[x.find('<') + 1:x.find('>')].replace('/','')
+        value = x[x.find('>')+1:x.find('</')]
+        structure[tag] = value
+    return structure
+
+def load_event_types():
     winevts = {}
-    with open(ev_path, 'r') as f:
+    with open("C:\\Users\\Casim\\Documents\\2023\\Eventful\\events.txt", 'r') as f:
         lines = f.read().split('\n')
     f.close()
     for line in lines:
         fields = line.split('\t')
-        id = fields[0].replace(' ', '')
-        info = ' '.join(fields[3:])
+        provider = fields[0]
+        id = fields[1].replace(' ', '')
+        info = f"{provider}: {' '.join(fields[2:])}"
         if id not in winevts.keys():
             winevts[id] = info
     return winevts
 
 
-def get_events(category):
-    events = []
-    cmd = f'Get-EventLog "{category}"'
-    for ln in run(f'powershell -command {cmd}').split('\n'):
-        fields = non_empty(ln.split(' '))
-        # index , time, entryType, source, id, message
-        if len(fields) > 6:
-            index = fields[0]
-            tstamp = ' '.join(fields[1:4])
-            eventType = fields[4]
-            source = fields[5]
-            message = ' '.join(fields[7:])
-            events.append({'time': tstamp, 'type': eventType, 'source': source, 'info': message})
-    open(f'{category}.json', 'w').write(json.dumps(events,indent=2))
-    return events
-
-
 def main():
-    winevts = load_event_types('events.txt')
-    apps = get_events('Application')
-    system = get_events("System")
-    get_events("Security")
+    event_log_dir = 'C:\\Windows\\System32\\winevt\\Logs'
+    root = "C:\\Users\\Casim\\Documents\\2023\\Eventful"
+
+    fout = f"{time.strftime('%m_%d_%y_%H%M%S')}"
+    os.mkdir(os.path.join(root, fout))
+    for log in os.listdir(event_log_dir):
+        print(f'[-] Parsing {log}')
+        parsed = log_read(os.path.join(event_log_dir,log))
+        if len(parsed):
+            print(f'\tsaving results')
+            open(os.path.join(fout,f'parsed_{log.split(".")[0]}.json'), 'w').write(json.dumps(parsed, indent=2))
 
 
 if __name__ == '__main__':
     main()
+
